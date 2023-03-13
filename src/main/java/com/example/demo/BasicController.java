258979c7d72e7029b6a5d3b2679e9461c5d887b6
Michele Junior Cioffo <michelecioffo_98@hotmail.com>
2023-03-13 17:18:40 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/BasicController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+@RestController
+@RequestMapping("somma")
+public class BasicController {
+
+    @GetMapping("")
+    public int getSum(){
+        Random rand = new Random();
+        int a = rand.nextInt(100);
+        int b = rand.nextInt(100);
+        return a + b;
+    }
+}
